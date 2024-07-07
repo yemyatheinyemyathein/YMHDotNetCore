5ab9a2a1d96f27c871e1ae5e72188c9261d9e8dc
Ye Myat Hein <yemyathein67@gmail.com>
2024-07-07 23:11:05 +0630

    [Complete] Ajax CRUD

---
+++ YMHDotNetCore.MvcApp2/Db/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YMHDotNetCore.MvcApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMHDotNetCore.MvcApp2.Db;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+    //}
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ YMHDotNetCore.MvcApp2/Models/BlogModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YMHDotNetCore.MvcApp2.Models;
+[Table("Tbl_Blog")]
+public class BlogModel
+{
+    [Key]
+    public int BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+}
+
+public class MessageModel
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+}
