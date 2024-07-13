67a2b352a35dc948c2bc3dc9de705be0fb8c68cc
Ye Myat Hein <yemyathein67@gmail.com>
2024-07-13 20:01:08 +0630

    [complete] HighChart

---
+++ YMHDotNetCore.MvcApp2/Controllers/HighChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YMHDotNetCore.MvcApp2.Controllers
+{
+    public class HighChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
